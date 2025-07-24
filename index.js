5cd8db1632104ce1c7bd6aacdc369c4a72cd9fe7
Sebastian Martinez <Luismartinez24210@gmail.com>
2025-07-23 21:03:36 -0500

    Fix

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-const client = new Client();
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    }
+});
 
 client.on('ready', () => {
     console.log('Client is ready!');
